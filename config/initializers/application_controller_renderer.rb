73f21ba3314ddf2bb48bfc2a2711302405c73126
pochi13 <pochi.com777@gmail.com>
2021-08-28 05:56:18 +0900

    管理機能実装完了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0024a4b680919f07af46f9c88e784a0c73ee4b4ecca9f57984f5ec666456bd01697b1e1366060aa332076756ce13ca15cdc300e86ee97b78321d65820bebf084'
