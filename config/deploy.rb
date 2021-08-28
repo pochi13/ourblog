59b2390096e273db6cd17ffce203a09bb30ce201
pochi13 <pochi.com777@gmail.com>
2021-08-28 18:57:06 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.16.0"
 
 set :application, "ourblog"
-set :repo_url, "pochi13/ourblog.git"
+set :repo_url, "git@github.com:pochi13/ourblog.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
