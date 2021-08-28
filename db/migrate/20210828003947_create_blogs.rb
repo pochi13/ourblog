f16dde3c1a1225634e658a0a1e6560138aa2a97c
pochi13 <pochi.com777@gmail.com>
2021-08-28 11:17:52 +0900

    ブログ投稿機能実装完了

---
+++ db/migrate/20210828003947_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :blogs do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
