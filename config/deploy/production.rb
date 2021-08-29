c1508ac3934df401862d5edabca5080ba209d44d
pochi13 <pochi.com777@gmail.com>
2021-08-29 16:58:33 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '35.75.76.175', user: 'ec2-user', roles: %w{app db web}
+server '35.74.224.200', user: 'ec2-user', roles: %w{app db web}
