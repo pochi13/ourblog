bf26b5455dd80374ef263834f30a775b0c6887aa
pochi13 <pochi.com777@gmail.com>
2021-08-30 16:48:23 +0900

    コメント削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,14 @@ class CommentsController < ApplicationController
     redirect_to "/blogs/#{comment.blog.id}"
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @blog = @comment.blog
+    @comment.destroy
+    redirect_to blog_path(@blog)
+  end
+
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, blog_id: params[:blog_id])
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
   root to: "home#index"
   resources :users
   resources :blogs do
-    resources :comments, only: [:create,:delete]
+    resources :comments, only: [:create,:destroy]
     collection do
       get 'search'
     end
