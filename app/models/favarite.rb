fe50ab13c57b405a0c264b56d32e0637220a953b
pochi13 <pochi.com777@gmail.com>
2021-09-01 12:06:35 +0900

    修正

--- app/models/favarite.rb
+++ app/models/favarite.rb
@@ -1,5 +1,5 @@
 class Favarite < ApplicationRecord
   belongs_to :user
   belongs_to :blog
-  varidates_uniqueness_of :blog_id, scope: :user_id
+  validates_uniqueness_of :blog_id, scope: :user_id
 end
