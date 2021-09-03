faca8298378d0999f2b24e3b198e68227746010a
pochi13 <pochi.com777@gmail.com>
2021-09-03 21:21:28 +0900

    問い合わせフォーム実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
   Rails.application.routes.draw do
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  resources :contacts
 
 
+  resources :contacts
   devise_for :users
   root to: "home#index"
   resources :users
